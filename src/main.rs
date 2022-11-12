ee953a4af9d3798a92c96e4b5c1567047bf374e2
Danny Hadley <dadleyy@gmail.com>
2022-11-11 19:04:49 -0500

    swap to use rwlock

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,11 @@ use v4l::video::Capture;
 
 #[derive(Clone, Debug)]
 struct SharedState {
-  sender: async_std::channel::Sender<async_std::channel::Sender<Vec<u8>>>,
+  last_frame: async_std::sync::Arc<async_std::sync::RwLock<(std::time::Instant, Vec<u8>)>>,
 }
 
 async fn render(request: tide::Request<SharedState>) -> tide::Result<tide::Response> {
   log::info!("has request, sending sender");
-  let (sender, receiver) = async_std::channel::unbounded();
-  request.state().sender.send(sender).await?;
-
   let (mut writer, drain) = futures::channel::mpsc::channel::<io::Result<Vec<u8>>>(1);
   let buf_drain = futures::stream::TryStreamExt::into_async_read(drain);
 
@@ -24,28 +21,30 @@ async fn render(request: tide::Request<SharedState>) -> tide::Result<tide::Respo
     .build();
 
   async_std::task::spawn(async move {
+    let frame_reader = request.state().last_frame.read().await;
+    let mut last_frame = (*frame_reader).0.clone();
+    drop(frame_reader);
+
     loop {
-      match receiver.try_recv() {
-        Err(error) if error.is_empty() => continue,
-        Err(error) => {
-          log::warn!("failed receiving - {error}");
+      let frame_reader = request.state().last_frame.read().await;
+      if (*frame_reader).0 != last_frame {
+        last_frame = (*frame_reader).0.clone();
+
+        let mut buffer = format!(
+          "--boundarydonotcross\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
+          (*frame_reader).1.len()
+        )
+        .into_bytes();
+
+        buffer.extend_from_slice((*frame_reader).1.as_slice());
+        buffer.extend_from_slice(b"\r\n");
+
+        if let Err(error) = writer.try_send(Ok(buffer)) {
+          log::warn!("unable to send received data - {error}");
           break;
         }
-        Ok(data) => {
-          let mut buffer = format!(
-            "--boundarydonotcross\r\nContent-Type: image/jpeg\r\nContent-Length: {}\r\n\r\n",
-            data.len()
-          )
-          .into_bytes();
-          buffer.extend_from_slice(data.as_slice());
-          buffer.extend_from_slice(b"\r\n");
-
-          if let Err(error) = writer.try_send(Ok(buffer)) {
-            log::warn!("unable to send received data - {error}");
-            break;
-          }
-        }
-      };
+      }
+      drop(frame_reader);
     }
   });
 
@@ -82,10 +81,16 @@ async fn run(dev: Device) -> io::Result<()> {
     return Err(io::Error::new(io::ErrorKind::Other, "mjpg-format not supported"));
   }
 
-  let (sender, receiver) = async_std::channel::bounded(2);
+  let last_frame_index = async_std::sync::Arc::new(async_std::sync::RwLock::new((
+    std::time::Instant::now(),
+    Vec::with_capacity(1024),
+  )));
+  let mut server = tide::with_state(SharedState {
+    last_frame: last_frame_index.clone(),
+  });
 
   let reader_thread = async_std::task::spawn(async move {
-    let mut listeners: Vec<async_std::channel::Sender<Vec<u8>>> = vec![];
+    let frame_locker = last_frame_index.clone();
     let mut stream = v4l::prelude::MmapStream::with_buffers(&dev, v4l::buffer::Type::VideoCapture, 4).unwrap();
     let mut last_debug = std::time::Instant::now();
     let mut current_frames = 0;
@@ -96,6 +101,8 @@ async fn run(dev: Device) -> io::Result<()> {
       let after = std::time::Instant::now();
       current_frames += 1;
       let seconds_since = before.duration_since(last_debug).as_secs();
+      let mut writable_frame = frame_locker.write().await;
+      *writable_frame = (std::time::Instant::now(), buffer.to_vec());
 
       if seconds_since > 3 {
         let frame_read_time = after.duration_since(before).as_millis();
@@ -104,39 +111,8 @@ async fn run(dev: Device) -> io::Result<()> {
         last_debug = before;
         current_frames = 0;
       }
-
-      match receiver.try_recv() {
-        Err(error) if error.is_empty() => (),
-        Err(error) => {
-          log::warn!("unable to receive incoming listeners on reader thread - {error}");
-          break;
-        }
-        Ok(inner) => {
-          log::info!("has new listener, yay");
-          listeners.push(inner);
-        }
-      }
-
-      if !listeners.is_empty() {
-        let drained = listeners
-          .drain(0..)
-          .collect::<Vec<async_std::channel::Sender<Vec<u8>>>>();
-
-        for listener in drained.into_iter() {
-          match listener.send(buffer.to_vec()).await {
-            Ok(_) => listeners.push(listener),
-            Err(error) => {
-              log::warn!("unable to send - {error}");
-            }
-          }
-        }
-      }
     }
-
-    Ok(())
   });
-
-  let mut server = tide::with_state(SharedState { sender });
 
   server.at("/image").get(render);
 
